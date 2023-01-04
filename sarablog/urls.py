a4b4c993b6b7b283cf3f99cb425ac50269bab89b
Tony <tonyromeofit@gmail.com>
2023-01-04 10:33:40 -0500

    forms and user accounts

--- sarablog/urls.py
+++ sarablog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import SarablogListView,SarablogDetailView
+from .views import SarablogListView,SarablogDetailView,SarablogCreateView,SarablogUpdateView,SarablogDeleteView
 
 
 urlpatterns = [
+    path("post/new/", SarablogCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/", SarablogDetailView.as_view(), name="post_detail"),
     path("", SarablogListView.as_view(), name="home"),
-]
+    path("post/<int:pk>/edit/", SarablogUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", SarablogDeleteView.as_view(), name="post_delete")
+    ]
--- sarablog/views.py
+++ sarablog/views.py
@@ -1,16 +1,33 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here.
 
+
 class SarablogListView(ListView):
     model = Post
     template_name = "home.html"
-    
+
+
 class SarablogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
-    
 
-    
+
+class SarablogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class SarablogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+class SarablogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
